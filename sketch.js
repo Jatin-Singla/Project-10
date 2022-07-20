34a7c8f77e90e111a05f3c151bd0ab6c1b85b27a
Jatin-Singla <109407143+Jatin-Singla@users.noreply.github.com>
2022-07-20 14:45:19 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
   sea.velocityX = -3;
 
   //uncomment code to reset the background
-  if(sea.x < 0||mouseIsPressed(){
+  if(sea.x < 0||mouseDown(){
    //sea.x = 0;
     //sea.x = sea.width;
     sea.x = sea.width/8;
